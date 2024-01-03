0fc37113241ddd8001d3debfecb6305f5f0ebe07
Yangyu Chen <cyy@cyyself.name>
2024-01-03 17:34:24 +0800

    add w^x feature

--- stress.cpp
+++ stress.cpp
@@ -5,6 +5,8 @@
 #include <cstdlib>
 #include <random>
 
+#define PAGESIZE 16384
+
 void generate_set_value(int x, int *place) {
 #ifdef __riscv
     uint32_t instr[4] = {
@@ -21,16 +23,22 @@ void generate_set_value(int x, int *place) {
 }
 
 int main(int argc, char *argv[]) {
-    void* addr = mmap(NULL, 4096, PROT_READ | PROT_WRITE | PROT_EXEC, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
-    int test = 0;
     if (argc < 2) {
-        printf("usage: ./stress <num_loops>\n");
+        printf("usage: ./stress <num_loops> [use_w^x:0]\n");
         exit(0);
     }
+    bool use_w_xor_x = false;
+    if (argc >= 3) {
+        use_w_xor_x = atoi(argv[2]) ? true : false;
+    }
+    void* addr = mmap(NULL, PAGESIZE, PROT_READ | PROT_WRITE | (use_w_xor_x ? 0 : PROT_EXEC), MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+    int test = 0;
     for (int i=0;i<atoi(argv[1]);i++) {
         test = 0;
+        if(use_w_xor_x) mprotect(addr, PAGESIZE, PROT_READ | PROT_WRITE);
         generate_set_value(i % 2048, (int*)addr);
         asm volatile("fence.i");
+        if(use_w_xor_x) mprotect(addr, PAGESIZE, PROT_READ | PROT_EXEC);
         ((void(*)(int *x, int y))addr)(&test, 0);
     }
     return 0;
